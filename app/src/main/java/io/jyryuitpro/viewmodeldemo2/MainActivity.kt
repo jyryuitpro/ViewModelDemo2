cc22689980667cc44cf67aa6f8d8c68460f9a575
jyr <jyr@wavers.co.kr>
2022-09-03 17:00:57 +0900

    LiveData Fundamentals

--- app/src/main/java/io/jyryuitpro/viewmodeldemo2/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package io.jyryuitpro.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import io.jyryuitpro.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -17,11 +18,19 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-        binding.resultTextView.text = viewModel.getTotal().toString()
+
+//        binding.resultTextView.text = viewModel.getTotal().toString()
+
+        // initial
+        // viewModel.total.observe(this, Observer {
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
+
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
+//            binding.resultTextView.text = viewModel.getTotal().toString()
         }
     }
 }
--- app/src/main/java/io/jyryuitpro/viewmodeldemo2/MainActivityViewModel.kt
+++ app/src/main/java/io/jyryuitpro/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,20 +1,26 @@
 package io.jyryuitpro.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
 
-    private var total = 0
+//    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int>
+    get() = total
 
     init {
-        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal(): Int {
-        return total
-    }
+//    fun getTotal(): Int {
+//        return total
+//    }
 
     fun setTotal(input: Int) {
-        total += input
+//        total += input
+        total.value = (total.value)?.plus(input)
     }
 }
